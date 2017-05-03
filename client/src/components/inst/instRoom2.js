7cb15ead4e563e1a9855bb73ac535be4a51275e1
Caleb <caleb@theparker.me>
2017-05-03 09:26:04 -0700

    comp name fix1

---
+++ client/src/components/inst/instRoom2.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import '../../assets/styles/InstRoom.css'
+import {connect} from 'react-redux'
+import {addNewTopic} from '../../api/perci'
+import PieChart from '../charts/pieChart'
+
+
+class InstRoom extends Component {
+
+  constructor() {
+
+    super()
+
+    this.state = {
+
+      messages: ''
+
+    }
+  }
+
+
+
+  setNewTopic() {
+
+    var c = document.getElementById('InstRoomAddTopicInp').value
+
+    addNewTopic(c)
+
+    document.getElementById('InstRoomAddTopicInp').value = ''
+
+  }
+
+  render() {
+
+    return (
+
+      <div
+        id='InstRoomCont'>
+
+        <h1
+          id='InstRoomCurrTopic'>
+          {this.props.topic}
+        </h1>
+
+        <h4
+          id='InstRoomName'>
+          {this.props.room}
+        </h4>
+
+        <input 
+          id='InstRoomAddTopicInp'
+          type='text' 
+          name='roomname'
+          placeholder='Topic'/>
+
+        <div
+          id='SetTopicBtn'
+          onClick={this.setNewTopic}>
+        Set Topic
+        </div>
+
+          <PieChart />
+
+      </div>
+
+    )
+  }
+}
+
+
+
+const mapStateToProps = function(appState) {
+
+  return {
+
+    user: appState.user,
+    room: appState.room,
+    topic: appState.topic
+
+  }
+
+  
+
+}
+
+
+export default connect(mapStateToProps)(InstRoom)
+
+
+
